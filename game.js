f46f5867bb9ceb311b00134c0633ce386f62ee02
Austin Tsiaperas <liketexas@gmail.com>
2018-01-27 16:19:31 -0600

    it works slightly better now

--- game.js
+++ game.js
@@ -14,13 +14,16 @@ var player2;
 var thisPlayer;
 var otherPlayer;
 var RPS ={ R: 0, P: 1, S: 2};
+var theirChoice;
+var yourChoice;
 
 
-function Player() {
+function Player(id) {
     this.wins = 0;
     this.losses = 0;
     this.choice = "";
     this.messages = "";
+    this.id = id;
 }
 
 var data = firebase.database();
@@ -45,7 +48,7 @@ $(document).ready(function(){
         
         else
         {
-    	    player2 = new Player;
+    	    player2 = new Player("player2");
     	    otherPlayer = "player1"
     	    thisPlayer = "player2";
     	    data.ref("player2").set(player2);
@@ -62,7 +65,7 @@ $(document).ready(function(){
     
     else 
     {
-    	player1 = new Player;
+    	player1 = new Player("player1");
     	thisPlayer = "player1";
     	otherPlayer = "player2";
     	data.ref("player1").set(player1);
@@ -74,91 +77,96 @@ $(document).ready(function(){
 
 $(".rps").on("click", function()
 {
-	var choice = this.id;
-    console.log(choice);
-    data.ref().once("value").then(function(snapshot){
-    if (snapshot.child(otherPlayer+"/choice").val() == "")
+	yourChoice = this.id;
+    console.log(yourChoice);
+    data.ref().once("value").then(function(snapshot)
     {
-        if (snapshot.child(thisPlayer+"/choice").val() != "")
+        if (snapshot.child(otherPlayer+"/choice").val() == "")
         {
-            $("#sorry .modal-body").text("You already chose!");
-            $('#sorry').modal('show');
+            if (snapshot.child(thisPlayer+"/choice").val() != "")
+            {
+                $("#sorry .modal-body").text("You already chose!");
+                $('#sorry').modal('show');
+            }
+            else
+            {
+    	        data.ref(thisPlayer+"/choice").set(yourChoice);
+            }
         }
-        else
-        {
-    	    data.ref(thisPlayer+"/choice").set(choice);
-        }
-    }
         
-    else if (!snapshot.child(otherPlayer+"/choice").exists())
-    {
-    	if (snapshot.child(thisPlayer+"/choice").val() != "")
+        else if (!snapshot.child(otherPlayer+"/choice").exists())
         {
-            $("#sorry .modal-body").text("You already chose!");
-            $('#sorry').modal('show');
+    	    if (snapshot.child(thisPlayer+"/choice").val() != "")
+            {
+                $("#sorry .modal-body").text("You already chose!");
+                $('#sorry').modal('show');
+            }
+            else
+            {
+    	        data.ref(thisPlayer+"/choice").set(yourChoice);
+            }
         }
+
         else
         {
-    	    data.ref(thisPlayer+"/choice").set(choice);
-        }
-    }
-
-    else
-    {
-    	theirChoice = snapshot.child(otherPlayer+"/choice").val();
-        console.log(RPS[choice]);
-        console.log(RPS[theirChoice]);
-    	switch (RPS[choice]-RPS[theirChoice])
-    	{
-            case 0:
-                $("#results .modal-body").html("It was a tie!<br>");
+    	    theirChoice = snapshot.child(otherPlayer+"/choice").val();
+            console.log(RPS[yourChoice]);
+            console.log(RPS[theirChoice]);
+    	    switch (RPS[yourChoice]-RPS[theirChoice])
+    	    {
+                case 0:
+                // $("#results .modal-body").html("It was a tie!<br>");
                 break;
     	   
-            case 1:
-                $("#results .modal-body").html("You won!<br>");
+                case 1:
+                // $("#results .modal-body").html("You won!<br>");
                 var losses = snapshot.child(otherPlayer+"/losses").val() + 1;
     	        data.ref(otherPlayer+"/losses").set(losses);
     	        var wins = snapshot.child(thisPlayer+"/wins").val() + 1;
     	        data.ref(thisPlayer+"/wins").set(wins);
     	        break; 
 
-    	    case -2:
-                $("#results .modal-body").html("You won!<br>");
+                case -2:
+                // $("#results .modal-body").html("You won!<br>");
                 var losses = snapshot.child(otherPlayer+"/losses").val() + 1;
     	        data.ref(otherPlayer+"/losses").set(losses);
     	        var wins = snapshot.child(thisPlayer+"/wins").val() + 1;
     	        data.ref(thisPlayer+"/wins").set(wins);
     	        break;
 
-    	    case 2:
-    	         $("#results .modal-body").html("You lost!<br>");
+    	        case 2:
+    	         // $("#results .modal-body").html("You lost!<br>");
     	         var losses = snapshot.child(thisPlayer+"/losses").val() + 1;
     	         data.ref(thisPlayer+"/losses").set(losses);
     	         var wins = snapshot.child(otherPlayer+"/wins").val() + 1;
     	         data.ref(otherPlayer+"/wins").set(wins); 
                  break; 
 
-             case -1:
-    	         $("#results .modal-body").html("You lost!<br>");
+                case -1:
+    	         // $("#results .modal-body").html("You lost!<br>");
     	         var losses = snapshot.child(thisPlayer+"/losses").val() + 1;
     	         data.ref(thisPlayer+"/losses").set(losses);
     	         var wins = snapshot.child(otherPlayer+"/wins").val() + 1;
     	         data.ref(otherPlayer+"/wins").set(wins); 
                  break;              
-    	}
-        
-        choice = $("#"+choice).text();
-        theirChoice = $("#"+theirChoice).text();
+    	    }
+            data.ref(thisPlayer+"/choice").set("");
+            data.ref(otherPlayer+"/choice").set("");
+        }
+       
+    //     yourChoice = $("#"+yourChoice).text();
+    //     theirChoice = $("#"+theirChoice).text();
 
-    	$("#results .modal-body").append("You chose "+choice+"<br>");
-    	$("#results .modal-body").append("Your opponent chose "+theirChoice+"<br>");
-        $("#results").modal('show');
-        data.ref(thisPlayer+"/choice").set("");
-        data.ref(otherPlayer+"/choice").set("");
-    }
+    // 	$("#results .modal-body").append("You chose "+yourChoice+"<br>");
+    // 	$("#results .modal-body").append("Your opponent chose "+theirChoice+"<br>");
+
+    //     $("#results").modal('show');
+    //     data.ref(thisPlayer+"/choice").set("");
+    //     data.ref(otherPlayer+"/choice").set("");
     
-    $("#losses").text(snapshot.child(thisPlayer+"/losses").val()+"  ");
-    $("#wins").text(snapshot.child(thisPlayer+"/wins").val()+"  ");
+        $("#losses").text(" "+snapshot.child(thisPlayer+"/losses").val());
+        $("#wins").text(" "+snapshot.child(thisPlayer+"/wins").val());
+       
     });
 });
 
@@ -171,14 +179,60 @@ $(".rps").on("click", function()
 //     data.ref(thisPlayer).child("messages").push(input);
 // });
 
-// data.ref(otherPlayer).on("child_changed", function(childSnapshot) {
+data.ref().on("child_changed", function(childSnapshot) {
     
-//     console.log(childSnapshot.val());
+    console.log(childSnapshot.val());
+    var snap = childSnapshot.val();
+    console.log(snap.id);
+    if (snap.id != thisPlayer)
+    {
+        if (snap.choice)
+        {
+            theirChoice = snap.choice;
+            if (yourChoice)
+            {
+                switch (RPS[yourChoice]-RPS[theirChoice])
+                {
+                    case 0:
+                    $("#results .modal-body").html("It was a tie!<br>");
+                    break;
+           
+                    case 1:
+                    $("#results .modal-body").html("You won!<br>");
+                    break; 
 
-//     // if (childSnapshot.val() != "null")
-//     // {
-//     //     var line = "Them: "+childSnapshot.val();
-//     //     $("#messages").append(line);
-//     // }
+                    case -2:
+                    $("#results .modal-body").html("You won!<br>");
+                    break;
 
-// });
+                    case 2:
+                    $("#results .modal-body").html("You lost!<br>");
+                    break; 
+
+                    case -1:
+                    $("#results .modal-body").html("You lost!<br>");
+                    break;              
+                }
+       
+            yourChoice = $("#"+yourChoice).text();
+            theirChoice = $("#"+theirChoice).text();
+
+            $("#results .modal-body").append("You chose "+yourChoice+"<br>");
+            $("#results .modal-body").append("Your opponent chose "+theirChoice+"<br>");
+
+            $("#results").modal('show');
+            $("#losses").text(snap.child(thisPlayer+"/losses").val()+"  ");
+            $("#wins").text(snap.child(thisPlayer+"/wins").val()+"  ");
+            data.ref(thisPlayer+"/choice").set("");
+            data.ref(otherPlayer+"/choice").set("");
+            }
+        }
+    }
+    
+    // if (childSnapshot.val() != "null")
+    // {
+    //     var line = "Them: "+childSnapshot.val();
+    //     $("#messages").append(line);
+    // }
+
+});
